564b69c2de2a9b841e155b51fa5d05cb2907ffb5
Ankit Goyani <lanetteam.ankitg@gmail.com>
2017-01-17 16:58:56 +0530

    Inserting data

--- getData.js
+++ getData.js
@@ -1,6 +1,6 @@
 var db=require('./db');
 
-exports.get = function(res,id) {
+exports.getemp = function(id,res) {
         db.connect(function(err, con) {
             if(id==0)
             {
@@ -15,14 +15,56 @@ exports.get = function(res,id) {
                 con.query('select * from employees where id = ?',[id], function(err, result) {
                     if(err){console.log("select id error " + err.stack);}
                     con.release();
-                    console.log("sdfsdfheef            "+result);
+
                     debugger;
                     if(result.length != 0){
                         res.send(JSON.stringify(result));
                         return;
                     }
-                    res.send({status: 0, message: 'category detail not found.'});
+                    res.send({status: 0, message: 'Employee detail not found.'});
                 });
             }
         });
     };
+exports.getcoun = function(id,res) {
+    db.connect(function(err, con) {
+        if(id==0)
+        {
+            qry = 'select * from tblCountry';
+            con.query(qry, function(err, result) {
+                con.release();
+                res.send(JSON.stringify(result));
+                //console.log(JSON.stringify(result));
+            });
+        }
+        else {
+            con.query('select * from tblCountry where id = ?',[id], function(err, result) {
+                if(err){console.log("select id error " + err.stack);}
+                con.release();
+
+                debugger;
+                if(result.length != 0){
+                    res.send(JSON.stringify(result));
+                    return;
+                }
+                res.send({status: 0, message: 'Country not found.'});
+            });
+        }
+    });
+};
+exports.insertemp=function(emp,res){
+
+    db.connect(function (err,con){
+
+        con.query('insert into employees (name,location) values (?,?)',[emp.name,emp.location],function(err,result){
+            con.release();
+
+            if(err){
+                res.send({status:0,message:"Employees Insertion Failed"});
+            }
+            else{
+                res.send({status:1,message:"Employees Insertion Success"});
+            }
+        });
+    });
+};
