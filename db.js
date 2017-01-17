e8fc403d2403c5d564f7422dba384941fd37e87e
Ankit Goyani <lanetteam.ankitg@gmail.com>
2017-01-17 15:10:11 +0530

    Database Connection file

---
+++ db.js
@@ -0,0 +1,19 @@
+var mysql=require("mysql");
+
+var pool;
+
+exports.conn=function (){
+    this.pool=mysql.createPool({
+        host:'localhost',
+        user:'root',
+        password:'root',
+        port:3306,
+        database:'demo'
+    });
+};
+
+exports.connect= function(callback){
+    this.pool.getConnection (function(err,connection){
+        callback(err,connection);
+    });
+};
