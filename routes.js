ebac8a2882468ef0d14a15d85fc97ef268265731
Ankit Goyani <lanetteam.ankitg@gmail.com>
2017-01-17 16:58:29 +0530

    Inserting Endpoint

--- routes.js
+++ routes.js
@@ -2,9 +2,18 @@ var data=require("./getData");
 
 exports.route= function(app){
     app.get('/employee/',function(req,res){
-         data.get(res,0);
+         data.getemp(0,res);
     });
     app.get('/employee/:id',function(req,res){
-        data.get(res,req.params.id);
+        data.getemp(req.params.id,res);
+    });
+    app.get('/country/',function(req,res){
+        data.getcoun(0,res);
+    });
+    app.get('/country/:id',function(req,res){
+        data.getcoun(req.params.id,res);
+    });
+    app.post('/employee/',function(req,res){
+        data.insertemp(req.body,res);
     });
 };
