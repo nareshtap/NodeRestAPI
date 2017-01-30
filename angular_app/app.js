d7702a85c3d4143a6b6b9ea79639f66edaaa5bd8
Ankit Goyani <lanetteam.ankitg@gmail.com>
2017-01-30 17:23:28 +0530

    CRUD on Angular with ui-router

---
+++ angular_app/app.js
@@ -0,0 +1,5 @@
+angular.module("app", ['ui.router', 'home', 'add', 'edit', 'delete'])
+	.config(function() {})
+	.controller('IndexController', function($state) {
+		                     	$state.go('home');
+});
---
+++ angular_app/config.js
@@ -0,0 +1,7 @@
+config = {
+	"serverUrl" : "http://localhost:8080/",
+	"create":    "addemployee",
+	"read" :     "employeelist",
+	"update":    "editemployee",
+	"delete":    "deleteemployee"
+}
